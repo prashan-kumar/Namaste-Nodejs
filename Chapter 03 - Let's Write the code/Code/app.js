3a6b50ccf2f554b4e32783e2510dc4e57724cc13
Prashant sharma <prashantkumars584@gmail.com>
2025-01-18 23:28:06 +0530

    new code add

--- Chapter 03 - Let's Write the code/Code/app.js
+++ Chapter 03 - Let's Write the code/Code/app.js
@@ -2,9 +2,11 @@ const name = "Akshad Jaiswal"
 var a = 10;
 var b = 20;
 var c = a + b;
+var d= a*b;
 
 console.log(name);
 console.log(c);
+console.log(d); 
 
 // console.log(global)
 // The globalThis global property contains the global this value, which is usually akin to the global object.
