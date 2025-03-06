be9a7ac94f46672f1ebe9dc1d65d07c1b22149ea
Prashant sharma <prashantkumars584@gmail.com>
2025-03-06 16:31:25 +0530

    middleware creation

--- Chapter 13 - Creating a database & mongodb/Code/db.js
+++ Chapter 13 - Creating a database & mongodb/Code/db.js
@@ -2,46 +2,44 @@ const { MongoClient } = require("mongodb");
 const { ObjectId } = require("mongodb");
 
 //connection URL
-const url = "mongodb+srv://akshadsantoshjaiswal:rFw7NSxncTpSc7Bq@cluster0.yulixmn.mongodb.net/"
+const url= "mongodb+srv://prashantkumars584:bJ1GXStUU6qKmu7z@namasteproject.xuvvz.mongodb.net/"
 const client = new MongoClient(url);
 
-const dbname = "NamsteNodejs"
+const dbname = "mainDatabase"
 
 async function main() {
     await client.connect();
     console.log("Database connected successfully")
     const db = client.db(dbname);
-    const collection = db.collection("User")
+    const collection = db.collection("collection")
 
     const data = {
-        firstname: "Akshad",
-        lastname: "Jaiswal",
-        city: "Pune",
-        phoneNumber: "88526587",
+        firstname: "random",
+        lastname: "kumar"
     }
 
 
     // Create
-    // const insertData = await collection.insertMany([data])
-    // console.log("data inserted = ", insertData)
+    const insertData = await collection.insertMany([data])
+    console.log("data inserted = ", insertData)
 
 
-    // Update
-    // const updateData = await collection.updateOne({ _id: new ObjectId('67066d6a3be8f41630d5dae4') }, { $set: { firstname: "Mint" } })
-    // console.log("Updated document ", updateData)
+    // // Update
+    // // const updateData = await collection.updateOne({ _id: new ObjectId('67066d6a3be8f41630d5dae4') }, { $set: { firstname: "Mint" } })
+    // // console.log("Updated document ", updateData)
 
     
     //Read 
     const findData = await collection.find({}).toArray();
     console.log("All data :", findData)
 
-    //delete
-    // const deletedata = await collection.deleteOne({ _id: new ObjectId('670668562c6bd11e25050c13') })
-    // console.log("deleted data=>", deletedata)
+    // //delete
+    // // const deletedata = await collection.deleteOne({ _id: new ObjectId('670668562c6bd11e25050c13') })
+    // // console.log("deleted data=>", deletedata)
 
-    //Count documents
-    const countData = await collection.countDocuments({})
-    console.log("Number of documents in db are :", countData)
+    // //Count documents
+    // const countData = await collection.countDocuments({})
+    // console.log("Number of documents in db are :", countData)
 
 
 
